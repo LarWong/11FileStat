d19d1ce9a91a1bceb5f2d607833c82641460e8ae
Jason Tung <mickeu420@gmail.com>
2018-10-31 03:36:35 -0400

    sdlkjalka

---
+++ cmake-build-debug/CMakeFiles/11FileStat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/11FileStat.dir/filestat.c.o"
+  "11FileStat.pdb"
+  "11FileStat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/11FileStat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- filestat.c
+++ filestat.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/stat.h>
+#include <math.h>
+#include <string.h>
 
 int main(){
 
@@ -10,8 +12,32 @@ int main(){
 
     printf("\nPrinting stat info about this file...\n\n");
 
-    printf("\tFile Size: %llu\n",fileinfo.st_size);
+    off_t size = fileinfo.st_size;
+    char pref = 0;
+    long mult = 1;
+    char pref_ary[3] = "KMG";
+    for (int i = 1; i <= 3; i++){
+        if (size >= pow(1000,i)){
+            pref = pref_ary[i-1];
+            mult = pow(1000,i);
+        }
+    }
+
+//    mode_t mode = fileinfo.st_mode << (sizeof(fileinfo.st_mode) * 8 - 9);
+//    char * men2 = calloc(sizeof(char), 10);
+//    strcpy(men2,"rwxrwxrwx");
+//    for (int i = 0; i<9; i++){
+//        printf("%d\n",mode >> i);
+//        men2[i] = mode >> i ? men2[i] : '-';
+//    }
+//    printf("TESTING : %s\n", men2);
+
+    char * men = calloc(6,sizeof(char));
+    pref!=0 ? sprintf(men, "%lld%cB",size/mult,pref) : sprintf(men, "%lldB",size/mult);
+    printf("\tFile Size: %s\n",men);
+    //printf("\tFile Size: %llu\n",fileinfo.st_size);
     printf("\tFile Permissions(octal): %o\n",fileinfo.st_mode);
+    //printf("\tFile Permissions(octal): %o\n",fileinfo.st_mode);
     printf("\tTime of Last Access: %s\n",ctime(&fileinfo.st_atime));
 
     return 0;
